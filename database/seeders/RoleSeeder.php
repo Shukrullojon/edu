bdc2d16f56efe7de1c860b1601cb798aab9bf570
Shukrullojon <shukrullobk@gmail.com>
2024-06-05 15:52:11 +0500

    Permissions

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -15,16 +15,32 @@ class RoleSeeder extends Seeder
     public function run(): void
     {
         $permissions = Permission::pluck('id', 'id')->all();
-
         $admin = Role::create(['name' => 'Admin']);
         $admin->syncPermissions($permissions);
+
+        $permissions_teacher = Permission::whereIn('name',[
+            'attendance-index',
+            'home-profile'
+        ])->pluck('id', 'id')->all();
         $teacher = Role::create(['name' => 'Teacher']);
-        $teacher->syncPermissions($permissions);
+        $teacher->syncPermissions($permissions_teacher);
+
+        $permissions_reception = Permission::whereIn('name',[
+            'home-profile'
+        ])->pluck('id', 'id')->all();
         $reception = Role::create(['name' => 'Reception']);
-        $reception->syncPermissions($permissions);
+        $reception->syncPermissions($permissions_reception);
+
+        $permissions_finance = Permission::whereIn('name',[
+            'home-profile'
+        ])->pluck('id', 'id')->all();
         $finance = Role::create(['name' => 'Finance']);
-        $finance->syncPermissions($permissions);
+        $finance->syncPermissions($permissions_finance);
+
+        $permissions_student = Permission::whereIn('name',[
+            'home-profile'
+        ])->pluck('id', 'id')->all();
         $student = Role::create(['name' => 'Student']);
-        $student->syncPermissions($permissions);
+        $student->syncPermissions($permissions_student);
     }
 }
